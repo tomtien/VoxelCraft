55594ab4d06ad71aa961cba089c024974a5ea6c3
GamingBox <gamingbox156@gmail.com>
2022-08-15 16:58:22 +0200

    update

--- VoxelCraft/main.c
+++ VoxelCraft/main.c
@@ -78,35 +78,26 @@ int main()
 
     GLuint VAO, VBO, EBO;
 
-    // Generate the VAO, VBO, and EBO with only 1 object each
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
 
-    // Make the VAO the current Vertex Array Object by binding it
     glBindVertexArray(VAO);
 
-    // Bind the VBO specifying it's a GL_ARRAY_BUFFER
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    // Introduce the vertices into the VBO
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // Bind the EBO specifying it's a GL_ELEMENT_ARRAY_BUFFER
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    // Introduce the indices into the EBO
+
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    // Configure the Vertex Attribute so that OpenGL knows how to read the VBO
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-    // Enable the Vertex Attribute so that OpenGL knows to use it
+
     glEnableVertexAttribArray(0);
 
-    // Bind both the VBO and VAO to 0 so that we don't accidentally modify the VAO and VBO we created
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
-    // Bind the EBO to 0 so that we don't accidentally modify it
-    // MAKE SURE TO UNBIND IT AFTER UNBINDING THE VAO, as the EBO is linked in the VAO
-    // This does not apply to the VBO because the VBO is already linked to the VAO during glVertexAttribPointer
+
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     while (!glfwWindowShouldClose(window))
     {
